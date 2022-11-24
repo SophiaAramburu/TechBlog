78f5d1d5dc2a1f7c8e24b0b4972f53b54127bcb9
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-11-23 16:30:43 -0800

    created homeRoutes file

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,79 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "text", "user_id", "post_id", "created_at"],
+          include: { model: User, attributes: ["username"] },
+        },
+        { model: User, attributes: ["username"] },
+      ],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("dashboard", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "text", "created_at", "user_id", "post_id"],
+          include: { model: User, attributes: ["username"] },
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render("post", {
+      ...post,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+module.exports = router;
